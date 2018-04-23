c089e82836d864c528e7ddf890a41cdcfd6de0ea
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/StackedSummary/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraCharts;
+using DevExpress.XtraCharts.Designer;
+
+namespace StackedSummary {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        Dictionary<string, PointInfo> Totals { get; set; }
+        List<Color> SeriesColors = new List<Color> { Color.Yellow, Color.Orange, Color.LightYellow };
+
+        private void Form1_Load(object sender, EventArgs e) {
+            Totals = new Dictionary<string, PointInfo>();
+            this.chartControl.SeriesDataMember = "Month";
+            this.chartControl.SeriesTemplate.View = new StackedBarSeriesView();
+            this.chartControl.SeriesTemplate.ArgumentDataMember = "Section";
+            this.chartControl.SeriesTemplate.ValueDataMembers.AddRange(new string[] {"Value"});
+            this.chartControl.DataSource = CreateChartData();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            ((XYDiagram)this.chartControl.Diagram).DefaultPane.StackedBarTotalLabel.Visible = checkBox1.Checked;
+        }
+        private DataTable CreateChartData() {
+
+            DataTable table = new DataTable("Table1");
+
+            table.Columns.Add("Month", typeof(String));
+            table.Columns.Add("Section", typeof(String));
+            table.Columns.Add("Value", typeof(Int32));
+
+            table.Rows.Add(new object[] { "Jan", "Section1", 10 });
+            table.Rows.Add(new object[] { "Jan", "Section2", 20 });
+            table.Rows.Add(new object[] { "Feb", "Section1", -12 });
+            table.Rows.Add(new object[] { "Feb", "Section2", 30 });
+            table.Rows.Add(new object[] { "March", "Section1", 14 });
+            table.Rows.Add(new object[] { "March", "Section2", 25 });
+
+            return table;
+        }
+
+        
+    }
+
+    public class PointInfo {
+        public double ActualValue { get; set; }
+        public double DisplayValue { get; set; }
+    }
+}
---
+++ VB/StackedSummary/Form1.vb
@@ -0,0 +1,60 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Forms
+Imports DevExpress.XtraCharts
+Imports DevExpress.XtraCharts.Designer
+
+Namespace StackedSummary
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Property Totals() As Dictionary(Of String, PointInfo)
+        Private SeriesColors As New List(Of Color) From {Color.Yellow, Color.Orange, Color.LightYellow}
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+            Totals = New Dictionary(Of String, PointInfo)()
+            Me.chartControl.SeriesDataMember = "Month"
+            Me.chartControl.SeriesTemplate.View = New StackedBarSeriesView()
+            Me.chartControl.SeriesTemplate.ArgumentDataMember = "Section"
+            Me.chartControl.SeriesTemplate.ValueDataMembers.AddRange(New String() {"Value"})
+            Me.chartControl.DataSource = CreateChartData()
+        End Sub
+
+        Private Sub checkBox1_CheckedChanged(ByVal sender As Object, ByVal e As EventArgs) Handles checkBox1.CheckedChanged
+            CType(Me.chartControl.Diagram, XYDiagram).DefaultPane.StackedBarTotalLabel.Visible = checkBox1.Checked
+        End Sub
+        Private Function CreateChartData() As DataTable
+
+            Dim table As New DataTable("Table1")
+
+            table.Columns.Add("Month", GetType(String))
+            table.Columns.Add("Section", GetType(String))
+            table.Columns.Add("Value", GetType(Int32))
+
+            table.Rows.Add(New Object() { "Jan", "Section1", 10 })
+            table.Rows.Add(New Object() { "Jan", "Section2", 20 })
+            table.Rows.Add(New Object() { "Feb", "Section1", -12 })
+            table.Rows.Add(New Object() { "Feb", "Section2", 30 })
+            table.Rows.Add(New Object() { "March", "Section1", 14 })
+            table.Rows.Add(New Object() { "March", "Section2", 25 })
+
+            Return table
+        End Function
+
+
+    End Class
+
+    Public Class PointInfo
+        Public Property ActualValue() As Double
+        Public Property DisplayValue() As Double
+    End Class
+End Namespace
